1bb992821e2c91c3b60475d61cd5b110532845b6
mollie brooks <mollieebrooks@gmail.com>
2018-09-03 15:55:19 +0200

    test basic predict and saveload

--- selfisher/tests/testthat/test-basics.R
+++ selfisher/tests/testthat/test-basics.R
@@ -1,2 +1,27 @@
 stopifnot(require("testthat"),
           require("selfisher"))
+
+data(haddock)
+dat <- transform(haddock, tot=nfine+nwide, prop=nwide/(nfine+nwide))
+
+
+context("Very basic selfisher fitting")
+m0 <- selfisher(prop~Lengths, p=~0, total=tot, dat)
+m1 <- selfisher(prop~Lengths, p=~1, total=tot, dat)
+m2 <- selfisher(prop~Lengths, total=tot, dat, cover=FALSE)
+
+test_that("Fixed psplit=0.5", {
+	expect_equal(unname(fixef(m0)$p), 0)
+	expect_equal(unname(fixef(m0)$r), c(-36.314353,1.233999), tol=1e-3)
+	})
+
+test_that("Default is cover=FALSE and estimate psplit", {
+	expect_equal(fixef(m1), fixef(m2))
+	expect_equal(L50SR(m1), L50SR(m2))
+	expect_is(m0, "selfisher")
+	expect_is(m1, "selfisher")
+})
+	
+
+context("summary")
+expect_equal()
